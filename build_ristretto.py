1925ba9a203ebd74fce1fe7bca0df7bd24fde1ae
Bram Westerbaan <bram@westerbaan.name>
2020-01-02 12:47:33 +0000

    removes -mavx2 compile flag that causes "Illegal instruction"

--- build_ristretto.py
+++ build_ristretto.py
@@ -16,7 +16,7 @@ with open(os.path.join(curdir, "ristretto.h")) as f:
 with open(os.path.join(curdir, "ristretto.c")) as f:
     source = header + f.read()
             
-ffi.set_source("_ristretto",  source, extra_compile_args=["-O3", "-march=native", "-mavx2", "-fomit-frame-pointer", "-std=c99"])
+ffi.set_source("_ristretto",  source, extra_compile_args=["-O3", "-march=native", "-fomit-frame-pointer", "-std=c99"])
 ffi.cdef(header)
 
 ffi.compile()
